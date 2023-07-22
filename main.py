aa3a2468cd9212888807c8180f08c8e653151873
yurii <yuriydorosh2005@gmail.com>
2023-07-22 22:52:20 +0300

    REFACTOR: by black

--- main.py
+++ main.py
@@ -2,74 +2,102 @@ import tkinter as tk
 from tkinter import *
 
 
-#head
+# head
 
 root = Tk()
-root.title('calculator')
-root.geometry('500x805')
-root.iconbitmap('calculatorico.ico')
+root.title("calculator")
+root.geometry("500x805")
+root.iconbitmap("calculatorico.ico")
 root.resizable(False, False)
-root.wm_attributes('-topmost', 1)
-root.configure(bg='black')
+root.wm_attributes("-topmost", 1)
+root.configure(bg="black")
 
-#counting function
+# counting function
+
 
 def calculate(operation):
     global formula
 
-    if operation == 'C':
-        formula = ''
-    elif operation =='del':
+    if operation == "C":
+        formula = ""
+    elif operation == "del":
         formula = formula[0:-1]
-    elif operation == '=':
+    elif operation == "=":
         formula = str(eval(formula))
-    elif operation == '√':
-        formula =str((eval(formula)) ** .5)
-    elif operation == '∛':
-        formula =str((eval(formula)) ** .333333333334)
-    elif operation == '∜':
-        formula =str((eval(formula)) ** .25)
-    elif operation == '5√':
-        formula =str((eval(formula)) ** .2)         
-    elif operation == '%':
-        formula =str((eval(formula)) /100) 
-   
+    elif operation == "√":
+        formula = str((eval(formula)) ** 0.5)
+    elif operation == "∛":
+        formula = str((eval(formula)) ** 0.333333333334)
+    elif operation == "∜":
+        formula = str((eval(formula)) ** 0.25)
+    elif operation == "5√":
+        formula = str((eval(formula)) ** 0.2)
+    elif operation == "%":
+        formula = str((eval(formula)) / 100)
+
     else:
-        if formula == '0':
-            formula = ''
+        if formula == "0":
+            formula = ""
         formula += operation
     Label_text.configure(text=formula)
 
 
+# display window
 
-#display window
-
-formula = '0'
-Label_text=tk.Label(text=formula,font=('Roboto',30,'bold'), bg='black', fg='white')
-Label_text.place(x = 11, y = 50)
-
+formula = "0"
+Label_text = tk.Label(text=formula, font=("Roboto", 30, "bold"), bg="black", fg="white")
+Label_text.place(x=11, y=50)
 
 
+# buttons
 
-
-#buttons
-
-buttons = [ 'C','del','%' ,'=', '7', '8', '9', '-', '4', '5', '6' , '+' , '1', '2', '3', '/' , '0' , '-','.', '*' , '^2','^3' ,  '^4', '^5', '√', '∛', '∜', '5√', '3.14','(',')']
+buttons = [
+    "C",
+    "del",
+    "%",
+    "=",
+    "7",
+    "8",
+    "9",
+    "-",
+    "4",
+    "5",
+    "6",
+    "+",
+    "1",
+    "2",
+    "3",
+    "/",
+    "0",
+    "-",
+    ".",
+    "*",
+    "^2",
+    "^3",
+    "^4",
+    "^5",
+    "√",
+    "∛",
+    "∜",
+    "5√",
+    "3.14",
+    "(",
+    ")",
+]
 
 
 x = 18
 y = 140
 for button in buttons:
     get_lbl = lambda x=button: calculate(x)
-    Button(text=button,bg='green',fg='white',font=('Roboto', 20), command = get_lbl).place(x = x, y = y, width = 115, height = 79)
+    Button(
+        text=button, bg="green", fg="white", font=("Roboto", 20), command=get_lbl
+    ).place(x=x, y=y, width=115, height=79)
     x += 117
 
-    if x>400:
+    if x > 400:
         x = 18
         y += 81
-
-
-
 
 
 root.mainloop()
