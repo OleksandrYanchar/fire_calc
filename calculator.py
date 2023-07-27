8d9402fb4cba73e551b49e6f23f278a6260c4c47
yurii <yuriydorosh2005@gmail.com>
2023-07-27 16:46:56 +0300

    REFACTOR: OOP + black

--- calculator.py
+++ calculator.py
@@ -1,74 +1,76 @@
 import math
 
 
-# Function to check if the last character in the formula is an operator
-def is_last_char_operator(formula):
-    if len(formula) > 0 and formula[-1] in "+-*/^.":
-        return True
-    return False
+class Calculator:
+    def __init__(self) -> None:
+        """Initialize the Calculator with a default formula of '0'."""
+        self.formula = "0"
 
+    def is_last_char_operator(self) -> bool:
+        """Check if the last character in the formula is an operator."""
+        return len(self.formula) > 0 and self.formula[-1] in "+-*/^."
 
-# Function to check if the formula starts with a negative number
-def starts_with_negative(formula):
-    return formula.startswith("-") and len(formula) > 1 and formula[1] in "0123456789"
+    def starts_with_negative(self) -> bool:
+        """Check if the formula starts with a negative number."""
+        return (
+            self.formula.startswith("-")
+            and len(self.formula) > 1
+            and self.formula[1] in "0123456789"
+        )
 
+    def get_formula(self) -> str:
+        """Return the current formula."""
+        return self.formula
 
-# Counting function
-def calculate(operation, formula):
-    # Handling different calculator operations
-    if operation == "C":
-        formula = ""  # Clear the formula
-    elif operation == "del":
-        formula = formula[0:-1]  # Remove the last character from the formula
-    elif operation == "=":
-        formula = str(eval(formula))  # Evaluate the formula and update the result
-    elif operation == "√":
-        formula = str((eval(formula)) ** 0.5)  # Calculate square root of the formula
-    elif operation == "∛":
-        formula = str((eval(formula)) ** (1 / 3))  # Calculate cube root of the formula
-    elif operation == "n√":
-        formula += "**(1/"  # Add a placeholder for the n-th root operation
-    elif operation == "%":
-        formula = str((eval(formula)) / 100)  # Calculate percentage of the formula
-    elif operation == "^2":
-        formula = str((eval(formula)) ** 2)  # Calculate square of the formula
-    elif operation == "^3":
-        formula = str((eval(formula)) ** 3)  # Calculate cube of the formula
-    elif operation == "^":
-        if not is_last_char_operator(formula):
-            formula += "**"  # Add the exponentiation operator to the formula
-    elif operation == "π":
-        formula = str(eval(formula + "math.pi"))  # Add the value of pi to the formula
-    elif operation == "e":
-        formula = str(
-            eval(formula + "math.e")
-        )  # Add the value of Euler's number (e) to the formula
-    elif operation == "cos":
-        formula = str(
-            math.cos(math.radians(eval(formula)))
-        )  # Calculate cosine of the formula (in degrees)
-    elif operation == "sin":
-        formula = str(
-            math.sin(math.radians(eval(formula)))
-        )  # Calculate sine of the formula (in degrees)
-    elif operation == "tan":
-        formula = str(
-            math.tan(math.radians(eval(formula)))
-        )  # Calculate tangent of the formula (in degrees)
-    elif operation == "cot":
-        formula = str(
-            math.tan(math.radians(eval(formula)))
-        )  # Calculate cotangent of the formula (in degrees)
-    else:
-        # Handling regular number and operator inputs
-        if formula == "0" or starts_with_negative(formula):
-            formula = ""
-        elif operation in "+-*/.^" and (
-            is_last_char_operator(formula) or starts_with_negative(formula)
-        ):
-            formula = formula[:-1]
-        formula += operation
-        if formula == ".":
-            formula = "0."
+    def calculate(self, operation: str) -> None:
+        """Perform calculator computations based on the selected operation.
 
-    return formula
+        Args:
+            operation (str): The calculator operation to be performed.
+
+        Returns:
+            None: The result is stored in the 'formula' attribute of the Calculator instance.
+        """
+        if operation == "C":
+            self.formula = ""
+        elif operation == "del":
+            self.formula = self.formula[0:-1]
+        elif operation == "=":
+            self.formula = str(eval(self.formula))
+        elif operation == "√":
+            self.formula = str((eval(self.formula)) ** 0.5)
+        elif operation == "∛":
+            self.formula = str((eval(self.formula)) ** (1 / 3))
+        elif operation == "n√":
+            self.formula += "**(1/"
+        elif operation == "%":
+            self.formula = str((eval(self.formula)) / 100)
+        elif operation == "^2":
+            self.formula = str((eval(self.formula)) ** 2)
+        elif operation == "^3":
+            self.formula = str((eval(self.formula)) ** 3)
+        elif operation == "^":
+            if not self.is_last_char_operator():
+                self.formula += "**"
+        elif operation == "π":
+            self.formula = str(eval(self.formula + "math.pi"))
+        elif operation == "e":
+            self.formula = str(eval(self.formula + "math.e"))
+        elif operation == "cos":
+            self.formula = str(math.cos(math.radians(eval(self.formula))))
+        elif operation == "sin":
+            self.formula = str(math.sin(math.radians(eval(self.formula))))
+        elif operation == "tan":
+            self.formula = str(math.tan(math.radians(eval(self.formula))))
+        elif operation == "cot":
+            self.formula = str(1 / math.tan(math.radians(eval(self.formula))))
+        else:
+            if self.formula == "0" or self.starts_with_negative():
+                self.formula = ""
+            elif operation in "+-*/.^" and (
+                self.is_last_char_operator() or self.starts_with_negative()
+            ):
+                self.formula = self.formula[:-1]
+            self.formula += operation
+            if self.formula == ".":
+                self.formula = "0."
--- main.py
+++ main.py
@@ -1,56 +1,71 @@
 import tkinter as tk
-import math
-from calculator import calculate
+import platform
+from calculator import Calculator
 from consts import buttons
 
 
-# GUI Setup
-def create_gui():
-    root = tk.Tk()
-    root.title("Calculator")
-    root.geometry("850x805")
-    root.iconbitmap("calculatorico.ico")
-    root.resizable(False, False)
-    root.wm_attributes("-topmost", 1)
-    root.configure(bg="black")
-    return root
+class CalculatorApp:
+    def __init__(self) -> None:
+        """Initialize the CalculatorApp."""
+        self.root = tk.Tk()
+        self.root.title("Calculator")
+        self.root.geometry("850x805")
+        # Check if running on Windows to set the icon
+        if platform.system() == "Windows":
+            self.root.iconbitmap("calculatorico.ico")
+        self.root.resizable(False, False)
+        self.root.wm_attributes("-topmost", 1)
+        self.root.configure(bg="black")
 
+        self.calculator = Calculator()
+        self.label_text = self.create_display()
+        self.create_buttons()
 
-def create_display(root):
-    global formula
-    formula = "0"
-    label_text = tk.Label(
-        root, text=formula, font=("Roboto", 30, "bold"), bg="black", fg="white"
-    )
-    label_text.place(x=11, y=50)
-    return label_text
+    def create_display(self) -> tk.Label:
+        """Create a Label widget to display the calculator's formula."""
+        label_text = tk.Label(
+            self.root,
+            text=self.calculator.get_formula(),
+            font=("Roboto", 30, "bold"),
+            bg="black",
+            fg="white",
+        )
+        label_text.place(x=11, y=50)
+        return label_text
 
+    def create_button(self, text: str, x: int, y: int) -> None:
+        """Create a calculator button with the specified text and position."""
 
-def create_button(root, text, x, y):
-    def get_label():
-        global formula
-        formula = calculate(text, formula)
-        label_text.configure(text=formula)
+        def get_label():
+            self.calculator.calculate(text)
+            self.label_text.configure(text=self.calculator.get_formula())
 
-    tk.Button(
-        root, text=text, bg="green", fg="white", font=("Roboto", 20), command=get_label
-    ).place(x=x, y=y, width=115, height=79)
+        tk.Button(
+            self.root,
+            text=text,
+            bg="green",
+            fg="white",
+            font=("Roboto", 20),
+            command=get_label,
+        ).place(x=x, y=y, width=115, height=79)
 
+    def create_buttons(self) -> None:
+        """Create all the calculator buttons."""
+        x, y = 18, 140
+        for button in buttons:
+            self.create_button(button, x, y)
+            x += 117
 
-def create_buttons(root):
-    x, y = 18, 140
-    for button in buttons:
-        create_button(root, button, x, y)
-        x += 117
+            # Creating a new line of buttons if the previous one is full
+            if x > 800:
+                x = 18
+                y += 81
 
-        # Creating a new line of buttons if the previous one is full
-        if x > 800:
-            x = 18
-            y += 81
+    def run(self) -> None:
+        """Start the main window's event loop."""
+        self.root.mainloop()
 
 
 if __name__ == "__main__":
-    root = create_gui()
-    label_text = create_display(root)
-    create_buttons(root)
-    root.mainloop()
+    app = CalculatorApp()
+    app.run()
